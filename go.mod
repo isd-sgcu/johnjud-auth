c213adfac8646971e78af4fb0e353b57c3943cad
NitiwatOwen <nitiwat3478@gmail.com>
2023-12-21 07:37:37 +0700

    feat: init main program

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/isd-sgcu/johnjud-auth
 
 go 1.21.0
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.31.0 // indirect
+	golang.org/x/net v0.16.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231002182017-d307bd883b97 // indirect
+	google.golang.org/grpc v1.60.1 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+type operation func(ctx context.Context) error
+
+func gracefulShutdown(ctx context.Context, timeout time.Duration, ops map[string]operation) <-chan struct{} {
+	wait := make(chan struct{})
+	go func() {
+		s := make(chan os.Signal, 1)
+
+		signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+		sig := <-s
+
+		log.Info().
+			Str("service", "graceful shutdown").
+			Msgf("got signal \"%v\" shutting down service", sig)
+
+		timeoutFunc := time.AfterFunc(timeout, func() {
+			log.Error().
+				Str("service", "graceful shutdown").
+				Msgf("timeout %v ms has been elapsed, force exit", timeout.Milliseconds())
+			os.Exit(0)
+		})
+
+		defer timeoutFunc.Stop()
+
+		var wg sync.WaitGroup
+
+		for key, op := range ops {
+			wg.Add(1)
+			innerOp := op
+			innerKey := key
+			go func() {
+				defer wg.Done()
+
+				log.Info().
+					Str("service", "graceful shutdown").
+					Msgf("cleaning up: %v", innerKey)
+				if err := innerOp(ctx); err != nil {
+					log.Error().
+						Str("service", "graceful shutdown").
+						Err(err).
+						Msgf("%v: clean up failed: %v", innerKey, err.Error())
+					return
+				}
+
+				log.Info().
+					Str("service", "graceful shutdown").
+					Msgf("%v was shutdown gracefully", innerKey)
+			}()
+		}
+
+		wg.Wait()
+		close(wait)
+	}()
+
+	return wait
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Str("service", "auth").
+			Msg("Failed to start service")
+	}
+
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	go func() {
+		log.Info().
+			Str("service", "auth").
+			Msg("JohnJud auth starting at port 8081")
+
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatal().
+				Err(err).
+				Str("service", "auth").
+				Msg("Failed to start service")
+		}
+	}()
+
+	wait := gracefulShutdown(context.Background(), 2*time.Second, map[string]operation{
+		"server": func(ctx context.Context) error {
+			grpcServer.GracefulStop()
+			return nil
+		},
+	})
+
+	<-wait
+
+	grpcServer.GracefulStop()
+	log.Info().
+		Str("service", "auth").
+		Msg("Closing the listener")
+	lis.Close()
+	log.Info().
+		Str("service", "auth").
+		Msg("End the program")
+}
