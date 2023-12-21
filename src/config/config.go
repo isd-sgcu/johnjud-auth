503052071afb340ba2bb70c78507e437e05b37cd
NitiwatOwen <nitiwat3478@gmail.com>
2023-12-21 08:19:53 +0700

    feat: add database config

--- src/config/config.go
+++ src/config/config.go
@@ -5,6 +5,15 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Database struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Name     string `mapstructure:"name"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	SSL      string `mapstructure:"ssl"`
+}
+
 type App struct {
 	Port   int    `mapstructure:"port"`
 	Debug  bool   `mapstructure:"debug"`
@@ -12,7 +21,8 @@ type App struct {
 }
 
 type Config struct {
-	App App `mapstructure:"app"`
+	App      App      `mapstructure:"app"`
+	Database Database `mapstructure:"database"`
 }
 
 func LoadConfig() (config *Config, err error) {
