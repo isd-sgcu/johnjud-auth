e83b37e15e0716654a72565d6dd8f10e976d1b19
NitiwatOwen <nitiwat3478@gmail.com>
2023-12-22 07:10:53 +0700

    feat: add User model

---
+++ src/internal/domain/model/user.model.go
@@ -0,0 +1,11 @@
+package model
+
+type User struct {
+	Base
+	Email        string `json:"email" gorm:"type:tinytext, unique"`
+	Password     string `json:"password" gorm:"type:tinytext"`
+	Firstname    string `json:"firstname" gorm:"type:tinytext"`
+	Lastname     string `json:"lastname" gorm:"type:tinytext"`
+	Role         string `json:"role" gorm:"type:tinytext"`
+	RefreshToken string `json:"refresh_token" gorm:"index"`
+}
