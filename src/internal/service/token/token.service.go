1f4b6864ed3fa2f14c8d7c50eab6cc0800ba54d7
NitiwatOwen <nitiwat3478@gmail.com>
2023-12-23 20:04:50 +0700

    feat: add CreateRefreshToken method and uuidUtil

--- src/internal/service/token/token.service.go
+++ src/internal/service/token/token.service.go
@@ -3,16 +3,18 @@ package token
 import (
 	"github.com/isd-sgcu/johnjud-auth/src/internal/constant"
 	tokenDto "github.com/isd-sgcu/johnjud-auth/src/internal/domain/dto/token"
+	"github.com/isd-sgcu/johnjud-auth/src/internal/utils"
 	"github.com/isd-sgcu/johnjud-auth/src/pkg/service/jwt"
 	authProto "github.com/isd-sgcu/johnjud-go-proto/johnjud/auth/auth/v1"
 )
 
 type serviceImpl struct {
 	jwtService jwt.Service
+	uuidUtil   utils.IUuidUtil
 }
 
-func NewService(jwtService jwt.Service) *serviceImpl {
-	return &serviceImpl{jwtService: jwtService}
+func NewService(jwtService jwt.Service, uuidUtil utils.IUuidUtil) *serviceImpl {
+	return &serviceImpl{jwtService: jwtService, uuidUtil: uuidUtil}
 }
 
 func (s *serviceImpl) CreateCredential(userId string, role constant.Role) (*authProto.Credential, error) {
@@ -21,4 +23,8 @@ func (s *serviceImpl) CreateCredential(userId string, role constant.Role) (*auth
 
 func (s *serviceImpl) Validate(token string) (*tokenDto.UserCredential, error) {
 	return nil, nil
+}
+
+func (s *serviceImpl) CreateRefreshToken() string {
+	return s.uuidUtil.GetNewUUID().String()
 }
--- src/pkg/service/token/token.service.go
+++ src/pkg/service/token/token.service.go
@@ -4,6 +4,7 @@ import (
 	"github.com/isd-sgcu/johnjud-auth/src/internal/constant"
 	tokenDto "github.com/isd-sgcu/johnjud-auth/src/internal/domain/dto/token"
 	"github.com/isd-sgcu/johnjud-auth/src/internal/service/token"
+	"github.com/isd-sgcu/johnjud-auth/src/internal/utils"
 	"github.com/isd-sgcu/johnjud-auth/src/pkg/service/jwt"
 	authProto "github.com/isd-sgcu/johnjud-go-proto/johnjud/auth/auth/v1"
 )
@@ -11,8 +12,9 @@ import (
 type Service interface {
 	CreateCredential(userId string, role constant.Role) (*authProto.Credential, error)
 	Validate(token string) (*tokenDto.UserCredential, error)
+	CreateRefreshToken() string
 }
 
-func NewService(jwtService jwt.Service) Service {
-	return token.NewService(jwtService)
+func NewService(jwtService jwt.Service, uuidUtil utils.IUuidUtil) Service {
+	return token.NewService(jwtService, uuidUtil)
 }
