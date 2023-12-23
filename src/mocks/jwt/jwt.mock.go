477cf1e76e8bc31824b6dc0decb1935f37c26ace
NitiwatOwen <nitiwat3478@gmail.com>
2023-12-23 19:07:00 +0700

    feat: create jwtServiceMock

---
+++ src/mocks/jwt/jwt.mock.go
@@ -0,0 +1,34 @@
+package jwt
+
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/isd-sgcu/johnjud-auth/src/config"
+	"github.com/stretchr/testify/mock"
+)
+
+type JwtServiceMock struct {
+	mock.Mock
+}
+
+func (m *JwtServiceMock) SignAuth(userId string) (string, error) {
+	args := m.Called(userId)
+	if args.Get(0) != "" {
+		return args.Get(0).(string), nil
+	}
+
+	return "", args.Error(1)
+}
+
+func (m *JwtServiceMock) VerifyAuth(token string) (*jwt.Token, error) {
+	args := m.Called(token)
+	if args.Get(0) != nil {
+		return args.Get(0).(*jwt.Token), nil
+	}
+
+	return nil, args.Error(1)
+}
+
+func (m *JwtServiceMock) GetConfig() *config.Jwt {
+	args := m.Called()
+	return args.Get(0).(*config.Jwt)
+}
