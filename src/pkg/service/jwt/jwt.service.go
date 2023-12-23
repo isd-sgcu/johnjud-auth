443eb0eedf752e0f276f67aed7104b472b2a8f17
NitiwatOwen <nitiwat3478@gmail.com>
2023-12-23 20:24:26 +0700

    fix: SignAuth signature

--- src/pkg/service/jwt/jwt.service.go
+++ src/pkg/service/jwt/jwt.service.go
@@ -3,13 +3,14 @@ package jwt
 import (
 	_jwt "github.com/golang-jwt/jwt/v4"
 	"github.com/isd-sgcu/johnjud-auth/src/config"
+	"github.com/isd-sgcu/johnjud-auth/src/internal/constant"
 	"github.com/isd-sgcu/johnjud-auth/src/internal/service/jwt"
 	"github.com/isd-sgcu/johnjud-auth/src/internal/utils"
 	"github.com/isd-sgcu/johnjud-auth/src/pkg/strategy"
 )
 
 type Service interface {
-	SignAuth(userId string, role string) (string, error)
+	SignAuth(userId string, role constant.Role) (string, error)
 	VerifyAuth(token string) (*_jwt.Token, error)
 	GetConfig() *config.Jwt
 }
