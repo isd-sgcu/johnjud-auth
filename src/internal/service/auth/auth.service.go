54f7d4d17869bb40f6ee34fdf7a7031c0498a6b5
NitiwatOwen <nitiwat3478@gmail.com>
2023-12-24 18:17:35 +0700

    feat: implement signup method

--- src/internal/service/auth/auth.service.go
+++ src/internal/service/auth/auth.service.go
@@ -2,10 +2,16 @@ package auth
 
 import (
 	"context"
+	"github.com/isd-sgcu/johnjud-auth/src/internal/constant"
+	"github.com/isd-sgcu/johnjud-auth/src/internal/domain/model"
 	"github.com/isd-sgcu/johnjud-auth/src/internal/utils"
 	userRepo "github.com/isd-sgcu/johnjud-auth/src/pkg/repository/user"
 	"github.com/isd-sgcu/johnjud-auth/src/pkg/service/token"
 	authProto "github.com/isd-sgcu/johnjud-go-proto/johnjud/auth/auth/v1"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
 )
 
 type serviceImpl struct {
@@ -32,10 +38,33 @@ func (s *serviceImpl) RefreshToken(_ context.Context, request *authProto.Refresh
 }
 
 func (s *serviceImpl) Signup(_ context.Context, request *authProto.SignupRequest) (*authProto.SignupResponse, error) {
-	// hash password with bcrypt
-	// initialize model.User
-	// create User in db
-	return nil, nil
+	hashPassword, err := s.bcryptUtil.GenerateHashedPassword(request.Password)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Internal server error")
+	}
+
+	createUser := &model.User{
+		Email:        request.Email,
+		Password:     hashPassword,
+		Firstname:    request.FirstName,
+		Lastname:     request.LastName,
+		Role:         constant.USER,
+		RefreshToken: "",
+	}
+	err = s.userRepo.Create(createUser)
+	if err != nil {
+		if errors.Is(err, gorm.ErrDuplicatedKey) {
+			return nil, status.Error(codes.AlreadyExists, "Duplicate email")
+		}
+		return nil, status.Error(codes.Internal, "Internal server error")
+	}
+
+	return &authProto.SignupResponse{
+		Id:        createUser.ID.String(),
+		FirstName: createUser.Firstname,
+		LastName:  createUser.Lastname,
+		Email:     createUser.Email,
+	}, nil
 }
 
 func (s *serviceImpl) SignIn(_ context.Context, request *authProto.SignInRequest) (*authProto.SignInResponse, error) {
--- src/internal/service/auth/auth.service_test.go
+++ src/internal/service/auth/auth.service_test.go
@@ -1,25 +1,179 @@
 package auth
 
 import (
+	"context"
+	"github.com/go-faker/faker/v4"
+	"github.com/google/uuid"
+	"github.com/isd-sgcu/johnjud-auth/src/internal/constant"
+	"github.com/isd-sgcu/johnjud-auth/src/internal/domain/model"
+	"github.com/isd-sgcu/johnjud-auth/src/mocks/repository/user"
+	"github.com/isd-sgcu/johnjud-auth/src/mocks/service/token"
+	"github.com/isd-sgcu/johnjud-auth/src/mocks/utils"
+	authProto "github.com/isd-sgcu/johnjud-go-proto/johnjud/auth/auth/v1"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
 	"testing"
+	"time"
 )
 
 type AuthServiceTest struct {
 	suite.Suite
+	ctx           context.Context
+	signupRequest *authProto.SignupRequest
 }
 
 func TestAuthService(t *testing.T) {
 	suite.Run(t, new(AuthServiceTest))
+
 }
 
-func (t *AuthServiceTest) SetupTest() {}
+func (t *AuthServiceTest) SetupTest() {
+	ctx := context.Background()
+	signupRequest := &authProto.SignupRequest{
+		FirstName: faker.FirstName(),
+		LastName:  faker.LastName(),
+		Email:     faker.Email(),
+		Password:  faker.Password(),
+	}
 
-func (t *AuthServiceTest) TestSignupSuccess() {}
+	t.ctx = ctx
+	t.signupRequest = signupRequest
+}
 
-func (t *AuthServiceTest) TestSignupHashPasswordFailed() {}
+func (t *AuthServiceTest) TestSignupSuccess() {
+	hashedPassword := faker.Password()
+	newUser := &model.User{
+		Email:        t.signupRequest.Email,
+		Password:     hashedPassword,
+		Firstname:    t.signupRequest.FirstName,
+		Lastname:     t.signupRequest.LastName,
+		Role:         constant.USER,
+		RefreshToken: "",
+	}
+	createdUser := &model.User{
+		Base: model.Base{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+		Email:        newUser.Email,
+		Password:     newUser.Password,
+		Firstname:    newUser.Firstname,
+		Lastname:     newUser.Lastname,
+		Role:         newUser.Role,
+		RefreshToken: newUser.RefreshToken,
+	}
 
-func (t *AuthServiceTest) TestSignupCreateUserFailed() {}
+	expected := &authProto.SignupResponse{
+		Id:        createdUser.ID.String(),
+		FirstName: createdUser.Firstname,
+		LastName:  createdUser.Lastname,
+		Email:     createdUser.Email,
+	}
+
+	userRepo := user.UserRepositoryMock{}
+	tokenService := token.TokenServiceMock{}
+	bcryptUtil := utils.BcryptUtilMock{}
+
+	bcryptUtil.On("GenerateHashedPassword", t.signupRequest.Password).Return(hashedPassword, nil)
+	userRepo.On("Create", newUser).Return(createdUser, nil)
+
+	authSvc := NewService(&userRepo, &tokenService, &bcryptUtil)
+	actual, err := authSvc.Signup(t.ctx, t.signupRequest)
+
+	assert.Nil(t.T(), err)
+	assert.Equal(t.T(), expected.Id, actual.Id)
+}
+
+func (t *AuthServiceTest) TestSignupHashPasswordFailed() {
+	hashPasswordErr := errors.New("Hash password error")
+
+	expected := status.Error(codes.Internal, "Internal server error")
+
+	userRepo := user.UserRepositoryMock{}
+	tokenService := token.TokenServiceMock{}
+	bcryptUtil := utils.BcryptUtilMock{}
+
+	bcryptUtil.On("GenerateHashedPassword", t.signupRequest.Password).Return("", hashPasswordErr)
+
+	authSvc := NewService(&userRepo, &tokenService, &bcryptUtil)
+	actual, err := authSvc.Signup(t.ctx, t.signupRequest)
+
+	status, ok := status.FromError(err)
+
+	assert.Nil(t.T(), actual)
+	assert.Equal(t.T(), codes.Internal, status.Code())
+	assert.True(t.T(), ok)
+	assert.Equal(t.T(), expected.Error(), err.Error())
+}
+
+func (t *AuthServiceTest) TestSignupCreateUserDuplicateConstraint() {
+	hashedPassword := faker.Password()
+	newUser := &model.User{
+		Email:        t.signupRequest.Email,
+		Password:     hashedPassword,
+		Firstname:    t.signupRequest.FirstName,
+		Lastname:     t.signupRequest.LastName,
+		Role:         constant.USER,
+		RefreshToken: "",
+	}
+	createUserErr := gorm.ErrDuplicatedKey
+
+	expected := status.Error(codes.AlreadyExists, "Duplicate email")
+
+	userRepo := user.UserRepositoryMock{}
+	tokenService := token.TokenServiceMock{}
+	bcryptUtil := utils.BcryptUtilMock{}
+
+	bcryptUtil.On("GenerateHashedPassword", t.signupRequest.Password).Return(hashedPassword, nil)
+	userRepo.On("Create", newUser).Return(nil, createUserErr)
+
+	authSvc := NewService(&userRepo, &tokenService, &bcryptUtil)
+	actual, err := authSvc.Signup(t.ctx, t.signupRequest)
+
+	status, ok := status.FromError(err)
+
+	assert.Nil(t.T(), actual)
+	assert.Equal(t.T(), codes.AlreadyExists, status.Code())
+	assert.True(t.T(), ok)
+	assert.Equal(t.T(), expected.Error(), err.Error())
+}
+
+func (t *AuthServiceTest) TestSignupCreateUserInternalFailed() {
+	hashedPassword := faker.Password()
+	newUser := &model.User{
+		Email:        t.signupRequest.Email,
+		Password:     hashedPassword,
+		Firstname:    t.signupRequest.FirstName,
+		Lastname:     t.signupRequest.LastName,
+		Role:         constant.USER,
+		RefreshToken: "",
+	}
+	createUserErr := errors.New("Internal server error")
+
+	expected := status.Error(codes.Internal, "Internal server error")
+
+	userRepo := user.UserRepositoryMock{}
+	tokenService := token.TokenServiceMock{}
+	bcryptUtil := utils.BcryptUtilMock{}
+
+	bcryptUtil.On("GenerateHashedPassword", t.signupRequest.Password).Return(hashedPassword, nil)
+	userRepo.On("Create", newUser).Return(nil, createUserErr)
+
+	authSvc := NewService(&userRepo, &tokenService, &bcryptUtil)
+	actual, err := authSvc.Signup(t.ctx, t.signupRequest)
+
+	status, ok := status.FromError(err)
+
+	assert.Nil(t.T(), actual)
+	assert.Equal(t.T(), codes.Internal, status.Code())
+	assert.True(t.T(), ok)
+	assert.Equal(t.T(), expected.Error(), err.Error())
+}
 
 func (t *AuthServiceTest) TestSignInSuccess() {}
 
