2b370d94508ad6837dde9c8a59579f5594976394
Idhibhat Pankam <marginpankam@gmail.com>
2024-01-07 11:02:55 +0700

    feat: user svc findone add notfound

--- internal/constant/error.constant.go
+++ internal/constant/error.constant.go
@@ -4,3 +4,5 @@ const InvalidTokenErrorMessage = "Invalid token"
 const IncorrectEmailPasswordErrorMessage = "Incorrect email or password"
 const DuplicateEmailErrorMessage = "Duplicate email"
 const InternalServerErrorMessage = "Internal server error"
+
+const UserNotFoundErrorMessage = "User not found"
